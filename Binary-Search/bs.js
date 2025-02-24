301d4b588170d61be934ee9c4a17f6e4826892c5
Vijay Benal <vijay.benal@graphenesvc.com>
2025-02-24 21:26:22 +0530

    floorAndCeil

--- Binary-Search/bs.js
+++ Binary-Search/bs.js
@@ -48,22 +48,22 @@
 
 // console.log("higherBound", higherBound([3, 5, 8, 9, 15, 19], 9));
 
-// function floorAndCeil(nums, target) {
-//     let low = 0, high = nums.length - 1;
-//     let floor, ceil;
-//     while (low <= high) {
-//       let mid = Math.floor((low + high) / 2);
-//       if (nums[mid] <= target) {
-//         floor = nums[mid];
-//         low = mid + 1;
-//       } else if (nums[mid] >= target) {
-//         ceil = nums[mid];
-//         high = mid - 1;
-//       }
-//     }
-//     return [floor, ceil];
-//   }
-//   let nums = [3, 4, 4, 7, 8, 10],
-//     target = 5;
-//   console.log("floorAndCeil", floorAndCeil(nums, target));
+function floorAndCeil(nums, target) {
+    let low = 0, high = nums.length - 1;
+    let floor, ceil;
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+      if (nums[mid] <= target) {
+        floor = nums[mid];
+        low = mid + 1;
+      } else if (nums[mid] >= target) {
+        ceil = nums[mid];
+        high = mid - 1;
+      }
+    }
+    return [floor, ceil];
+  }
+  let nums = [3, 4, 4, 7, 8, 10],
+    target = 5;
+  console.log("floorAndCeil", floorAndCeil(nums, target));
   
